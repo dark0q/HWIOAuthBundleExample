d2de3439b020c2246c73ecf15adc58cb21696022
Dariusz Kowalski <kowalskidariusz1@gmail.com>
2017-03-11 18:28:42 +0100

    Installed FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
